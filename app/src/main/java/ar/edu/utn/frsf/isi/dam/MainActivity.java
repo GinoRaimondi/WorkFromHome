59721fb826e41cb3e219cf05e6be8cdb3f4052f7
GinoRaimondi <gino4812@gmail.com>
2016-09-22 21:24:29 -0300

    Se agrega material y actividad

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/MainActivity.java
@@ -1,21 +1,29 @@
 package ar.edu.utn.frsf.isi.dam;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
 public class MainActivity extends AppCompatActivity {
 
     ListView listview;
+    private static Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainActivity.context = getApplicationContext();
 
         listview = (ListView) findViewById(R.id.listView);
 
@@ -26,5 +34,34 @@ public class MainActivity extends AppCompatActivity {
         CustomAdapter adapter = new CustomAdapter(this,  Arrays.asList(trabajos), getResources());
         listview.setAdapter(adapter);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mi_menu_principal, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+      /*  public boolean onOptionsItemSelected(MenuItem item){
+
+            if (item.getItemId() == R.id.idMenu1)
+            {
+                Toast.makeText(this, "this is my Toast message!!! =)",
+                        Toast.LENGTH_LONG).show();
+            }*/
+
+        switch (item.getItemId())
+        {
+            case R.id.idMenu1:
+
+                Intent intent = new Intent(this,nuevaOferta.class);
+
+                startActivityForResult(intent,0);
+        }
+
+        return true;
     }
 }
---
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/nuevaOferta.java
@@ -0,0 +1,21 @@
+package ar.edu.utn.frsf.isi.dam;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.Arrays;
+
+/**
+ * Created by RAIMONDI on 22/09/2016.
+ */
+public class nuevaOferta extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nueva_oferta);
+
+    }
+}
